ddf361331c7a23891f4c55ea4ffe0bdcf5e279e6
mifmif <mifmif.com@gmail.com>
2020-03-22 18:26:52 +0100

    [YMI][Tayssir] : Create JS examples and TS models for data to be used #4

---
+++ resources/models/TypeScript Interface/caid.ts
@@ -0,0 +1,13 @@
+declare module namespace {
+
+    export interface RootObject {
+        id: string;
+        fullName: string;
+        phone: string;
+        city: string;
+        province: string;
+        circle: string;
+        caidat: string;
+    }
+
+}
---
+++ resources/models/TypeScript Interface/userRequest.ts
@@ -0,0 +1,19 @@
+declare module namespace {
+
+    export interface RootObject {
+        fullName: string;
+        cin: string;
+        cinRecto: string;
+        cinVerso: string;
+        phone: string;
+        address: string;
+        jobAddress: string;
+        jobType: string;
+        city: string;
+        province: string;
+        circle: string;
+        caidat: string;
+        m9ademName: string;
+    }
+
+}
