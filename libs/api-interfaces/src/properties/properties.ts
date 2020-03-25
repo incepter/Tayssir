78c2086914db3fe2feca51cbac64dc6be72d71ac
abdeljabar <aitbelkhalil@gmail.com>
2020-03-25 22:21:44 +0100

    add SMS Notification management service

---
+++ libs/api-interfaces/src/properties/properties.ts
@@ -0,0 +1,5 @@
+export const properties = {
+    accountSid: 'xxxxxxxxxxxxxxxxxxxxxxxxxxx', // Your Account SID from www.twilio.com/console
+    authToken: 'xxxxxxxxxxxxxxxxxxxxxxxxxxx'   // Your Auth Token from www.twilio.com/console
+
+};
