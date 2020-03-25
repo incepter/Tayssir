db118a57825ab43c81c1dfcca72e1801ff0c6bd7
mifmif <mifmif.com@gmail.com>
2020-03-25 19:13:32 +0100

    [YMI][Tayssir] : Data collection : Region , province, Circle, Commune #7
    SQL Postgres queries to create and fill tables of Region , province, Circle, Commune

---
+++ resources/Data/location/SQL/region.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS "region" (
+"id" VARCHAR (2) NULL,
+"code" VARCHAR (3) NULL,
+"name" VARCHAR (50) NULL,
+"name_ar" VARCHAR (50) NULL
+);
+
+INSERT INTO region VALUES
+('01','01.','Tanger-Tetouan-Al Hoceima','طنجة تطوان، الحسيمة'),
+('02','02.','Oriental','شرقي'),
+('03','03.','Fès-Meknès','فاس-مكناس'),
+('04','04.','Rabat-Salé-Kénitra','القنيطرة-الرباط سلا'),
+('05','05.','Béni Mellal-Khénifra','بني ملال-خنيفرة'),
+('06','06.','Grand Casablanca-Settat','الدار البيضاء-سطات'),
+('07','07.','Marrakech-Safi','مراكش الصافي'),
+('08','08.','Drâa-Tafilalet','درعة تافيلالت'),
+('09','09.','Souss-Massa',' سوس ماسة'),
+('10','10.','Guelmim-Oued Noun','كلميم واد نون'),
+('11','11.','Laayoune-Sakia El Hamra','العيون الساقية الحمراء');
