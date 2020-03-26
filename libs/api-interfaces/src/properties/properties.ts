e4c9d7d1129e35d7a4f45975f38c82280e2fff44
abdeljabar <aitbelkhalil@gmail.com>
2020-03-26 09:33:25 +0100

    update properties.ts

--- libs/api-interfaces/src/properties/properties.ts
+++ libs/api-interfaces/src/properties/properties.ts
@@ -1,9 +1,6 @@
 export const properties = {
-    // twilioNumber: '+0000000000',
-    // accountSid: 'xxxxxxxxxxxxxxxxxxxxxxxxxxx', // Your Account SID from www.twilio.com/console
-    // authToken: 'xxxxxxxxxxxxxxxxxxxxxxxxxxx',   // Your Auth Token from www.twilio.com/console
-    accountSid: 'AC627eac16c54f9899570604643b79d4e4', // Your Account SID from www.twilio.com/console
-    authToken: '16aa92209c6a580ea595438158b3484b', // Your Auth Token from www.twilio.com/console
-    twilioNumber: '+12075693670',
+    twilioNumber: '+0000000000',
+    accountSid: 'xxxxxxxxxxxxxxxxxxxxxxxxxxx', // Your Account SID from www.twilio.com/console
+    authToken: 'xxxxxxxxxxxxxxxxxxxxxxxxxxx',   // Your Auth Token from www.twilio.com/console
     messageBody: 'Bienvenue sur Tayssir, Votre code de confirmation est : '
 };
